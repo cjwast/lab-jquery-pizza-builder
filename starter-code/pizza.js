c4cc83962ffdc07f987067b98746bde02a571849
Charlie Aguirre <cj.wast@hotmail.com>
2019-05-16 23:30:24 -0500

    first commit :beers:

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,21 @@
 // Write your Pizza Builder JavaScript in this file.
+var app1 = new Vue({
+  el: '#app-1',
+  data: {
+    seenGP: true,
+    seenMushrom: true,
+    seenPepperoni: true,
+
+  },
+  methods: {
+    btnGreenPepper: function () {
+      this.seenGP = !this.seenGP;
+    },
+    btnMushroom: function () {
+      this.seenMushrom = !this.seenMushrom;
+    },
+    btnPepperoni: function () {
+      this.seenPepperoni = !this.seenPepperoni;
+    }
+  }
+})
